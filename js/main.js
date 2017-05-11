b45d3d11f0182566237326c3dd7a16779081effe
binh <technobia@gmail.com>
2017-05-12 00:06:24 +0700

    on open / close modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // function VanillaModal(config) {
 //     this._body = document.getElementsByTagName('body')[0];
 //     var modal = document.createElement('div');
-//     modal.id = "modal" + Date.now() * 9999;
+//     modal.id = 'modal' + Date.now() * 9999;
 //     modal.style = 'position: absolute;left: ' + screen.width / 2 + 'px;top: ' + screen.height / 2 + 'px;transform:translate(-100%, -100%);width: 300px;height: 300px;background-color: red;z-index: 9999999';
 //     this._modal = modal;
 //     this.defaultPosition = this._body.style.position;
@@ -11,9 +11,9 @@
 //
 // Object.assign(VanillaModal.prototype, {
 //     open: function() {
-//         this._body.style.position = "relative";
-//         this._body.style.overflow = "hidden";
-//         this._body.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+//         this._body.style.position = 'relative';
+//         this._body.style.overflow = 'hidden';
+//         this._body.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
 //         this._body.appendChild(this._modal);
 //     },
 //     close: function() {
@@ -23,13 +23,37 @@
 //         this._body.removeChild(this._modal);
 //     }
 // })
-function LazadaModal() {
-    this.$body = document.getElementById('popup');
-    console.log('hello Modal');
+(function() {
+    this.LazadaModal = function() {
+        this.$body = document.getElementById('popup');
+    };
+
+    LazadaModal.prototype.open = function() {
+        this.$body.className = 'open';
+    };
+
+    LazadaModal.prototype.close = function() {
+        this.$body.className = this.$body.className.replace('open', '');
+    };
+}());
+
+
+if (document.readyState !== 'loading') {
+    onLoad()
+} else {
+    document.addEventListener('DOMContentLoaded', onLoad)
 }
 
-LazadaModal.prototype.open = function() {
-    this.$body.className = 'open';
-};
+function onLoad() {
+    var openButton = document.getElementById('btn-open');
+    var closeButton = document.getElementById('btn-close');
+    var lazadaModal = new LazadaModal();
 
-new LazadaModal();
+    openButton.addEventListener('click', function() {
+        lazadaModal.open();
+    });
+
+    closeButton.addEventListener('click', function() {
+        lazadaModal.close();
+    });
+}
