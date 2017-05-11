7c0996f6be26cfe1db85a4e19dff27ebf0c76246
binh <technobia@gmail.com>
2017-05-12 01:57:17 +0700

    on submit and validate form

--- js/form.js
+++ js/form.js
@@ -1,6 +1,38 @@
 (function() {
     this.validateFormOnSubmit = function(form) {
-        console.log(form);
+        var error = '';
+        error += validateEmail(form.email);
+        error += validatePassword(form.password);
+
+        if (error.length > 0) {
+            alert(error);
+        } else {
+            alert('Login Success');
+        }
+
         return false;
     };
+
+    function validateEmail(email) {
+        var error = '';
+        if (email.value.length === 0) {
+            email.className = email.className + ' has-error';
+            error = '- Please enter your email \n';
+        } else {
+            email.className = email.className.replace(' has-error', '');
+        }
+        return error;
+    }
+
+    function validatePassword(password) {
+        var error = '';
+        var re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@!%*#?&])[A-Za-z\d$@!%*#?&]{3,}$/i.test(password.value);
+        if (!re) {
+            error = '- Password must have least one special symbol, one number and one letter \n';
+            password.className = password.className + ' has-error';
+        } else {
+            password.className = password.className.replace(' has-error', '');
+        }
+        return error;
+    }
 }());
