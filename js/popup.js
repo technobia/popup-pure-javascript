38a674edd333fc6d3cec12a9140a8361d7dec829
binh <technobia@gmail.com>
2017-05-12 02:05:57 +0700

    on reset form when open

--- js/popup.js
+++ js/popup.js
@@ -5,9 +5,18 @@
 
     LazadaModal.prototype.open = function() {
         this.$body.className = 'open';
+        resetForm();
     };
 
     LazadaModal.prototype.close = function() {
         this.$body.className = this.$body.className.replace('open', '');
     };
+
+    function resetForm() {
+        var inputTagList = document.getElementsByTagName('input');
+        for (var i = 0; i < inputTagList.length; i++) {
+            inputTagList[i].className = inputTagList[i].className.replace(' has-error', '');
+            inputTagList[i].value = '';
+        }
+    }
 }());
